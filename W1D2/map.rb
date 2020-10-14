ef9d29a530b3be56d755f1338595e37e7ff74be4
Luke Luciani <lukeluciani222@gmail.com>
2020-10-14 12:26:52 -0400

    Finshed Map class methods with delete(key) and show

--- W1D2/map.rb
+++ W1D2/map.rb
@@ -12,8 +12,11 @@ class Map
     end
     
     def delete(key)
+        @my_map.delete_if { |k, v| k == key }
+        @my_map
     end
     
     def show
+        @my_map.each { |k, v| puts "The Key is #{k} and value is #{v}" }
     end    
 end    
