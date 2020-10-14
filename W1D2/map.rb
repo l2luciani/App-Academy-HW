771b24fd15c176f91b7b4579534223176d2d6ffa
Luke Luciani <lukeluciani222@gmail.com>
2020-10-14 11:48:49 -0400

    Setting the instance vars for Map class

---
+++ W1D2/map.rb
@@ -0,0 +1,17 @@
+class Map
+    def initialize
+        @my_map = []
+    end
+    
+    def set(key, value)
+    end
+    
+    def get(key)
+    end
+    
+    def delete(key)
+    end
+    
+    def show
+    end    
+end    
